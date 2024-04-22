09fea9475f46570b30e634e063cd434dde8902ab
bittiboop <sstrelkova15@gmail.com>
2024-04-22 20:18:53 +0300

    added 3 ex

--- main.cpp
+++ main.cpp
@@ -9,6 +9,14 @@ int main() {
 	//x++/x-- - post increment
 	//++x/--x - pre increment
 
-	cout << pow(2, 3) << endl;
+	cout << "Calculation of the volume of the parallelepiped.\n" << "Enter the output data:\n" << endl;
+	float len, width, height;
+	cout << "Length (cm) -> ";
+	cin >> len;
+	cout << "Width (cm) -> ";
+	cin >> width;
+	cout << "Height (cm) -> ";
+	cin >> height;
+	cout << "Volume: " << len * width * height << " cm^3" << endl;
 	return 0;
 }
