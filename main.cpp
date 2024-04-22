b4edced6ac43e69ed0820290f8352bf845ea5ec4
bittiboop <sstrelkova15@gmail.com>
2024-04-22 18:33:05 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+//#include <math.h>
+using namespace std;
+
+int main() {
+	//++(+1) - increment operator
+	//--(-1) - increment operator
+	//x++/x-- - post increment
+	//++x/--x - pre increment
+
+	cout << pow(2, 3) << endl;
+	return 0;
+}
